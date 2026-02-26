c0c0730f593c3e697c747a0b09215193ef7bd2cb
Maple <168573768+yigezhihuimazidexie@users.noreply.github.com>
2026-02-26 11:56:59 +0800

    something

--- example_mods/BF-31world/custom_events/Flash.lua
+++
@@ -1,5 +0,0 @@
-function onEvent(name, value1, value2)
-	if name == 'Flash' then
-		cameraFlash('camGame', value1, value2, true)
-	end
-end
--- example_mods/BF-31world/stages/BF-31.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    makeAnimatedLuaSprite('3', '3', -700, 250)
-    addAnimationByPrefix('3', '3', 'idle', 15, true)
-	addLuaSprite('3', false)
-    scaleObject('3', 0.8, 0.8)
-end
